4cccd57b47e469a0d5bb7ea68b79bed97a585ed0
George O'Hara <georgeohara92@gmail.com>
2020-07-21 11:15:58 +0100

    changed settings path

--- include/settings.hpp
+++ include/settings.hpp
@@ -5,7 +5,7 @@
 
 namespace settings {
     std::string const static SETTINGS_PATH
-        { utilities::get_path_to_executable_folder() + "/../resources/branend" };
+        { utilities::get_path_to_executable_folder() + "/../.resources/branigan-settings" };
     void write_ending_time_to_settings(std::string time);
     std::string retrieve_end_time();
 }
